7bfad2acf4135010a66c74dd7bae1cdf39f707b5
Anca Mosoiu <anca@anca.tv>
2012-02-06 11:26:25 -0800

    updates to the theme...  Lots of them

--- functions.php
+++ functions.php
@@ -11,10 +11,14 @@ define( 'CHILD_THEME_NAME', 'Tech Limial Child Theme' );
 define( 'CHILD_THEME_URL', 'http://techliminal.com' );
 
 /** Add new image sizes */
-add_image_size( 'home-bottom', 170, 90, TRUE );
-add_image_size( 'home-middle', 265, 150, TRUE );
-add_image_size( 'home-mini', 50, 50, TRUE );
+add_image_size( 'home-middle', 300, 120, TRUE );
+add_image_size( 'home-mini', 60, 60, TRUE );
  
+// Turn on random header image rotation by default.
+add_theme_support( 'custom-header', array( 'random-default' => true ) );
+
+// Add support for custom backgrounds
+add_custom_background();
  
 /* --------------------  Widget Areas ------------------------ */
 
@@ -28,7 +32,7 @@ genesis_register_sidebar(array(
 genesis_register_sidebar(array(
   'id' => 'home-main-right', 
   'name'=>'Home Main Right',
-  'description' => 'This is the right columns of the homepage',
+  'description' => 'This is the left main feature of the homepage',
   'before_title'=>'<h2 class="widgettitle">','after_title'=>'</h2>'
 )); 
 
@@ -40,15 +44,28 @@ genesis_register_sidebar(array(
 )); 
 
 /** Add support for 4 columns of footer widgets */
-add_theme_support( 'genesis-footer-widgets', 4 );
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+
+remove_action('genesis_header', 'genesis_do_header');
+add_action('genesis_header', 'tl_header');
 
 function tl_header(){
 ?>
-  <div id="logo"><a href="/"><img src="/images/logo-web.png" height=72 width=198 alt="Homepage" title="Tech Liminal" align="left"/></a>
-	<img src="/images/tagline.png" height=18 width=198 alt="Technology Hotspot and Salon"/>
+  <div id="logo">
+  	<a href="/"><img src="/wp-content/themes/techliminal/assets/img/logo-web.png" height=72 width=198 alt="Homepage" title="Tech Liminal" align="left"/>
+		<img src="/wp-content/themes/techliminal/assets/img/tagline.png" height=18 width=198 alt="Technology Hotspot and Salon"/></a>
 	</div>
-	
-	<div id="cinfo">268 14th Street<br/>Oakland, CA 94612<br/>info@techliminal.com<br/>(510) 832-3401</div>
+  <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress" id="contact_info">
+  	<h4 class="widgettitle">Find Us</h4>
+    <span itemprop="streetAddress"><a href="/find-us" title="Get a map and directions">268 14th Street</a></span><br/>
+    <span itemprop="addressLocality">Oakland</span>,
+    <span itemprop="addressRegion">CA</span>
+    <span itemprop="postalCode">94612</span><br/>
+    <span itemprop="telephone">(510) 832-3401</span><br/>
+  	<span itemprop="email"><a href="/contact-us" >info@techliminal.com</a></span>
+
+  </div>
 <?
 }
 
